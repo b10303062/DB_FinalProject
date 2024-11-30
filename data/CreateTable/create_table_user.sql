3095d628c602f57a9ed728337a68a324353ae939
panisleepy <151904238+panisleepy@users.noreply.github.com>
2024-11-30 13:30:39 +0800

    Update create_table_user.sql

--- data/CreateTable/create_table_user.sql
+++ data/CreateTable/create_table_user.sql
@@ -1,8 +1,8 @@
--- Table: public.User
+-- Table: public.user
 
--- DROP TABLE IF EXISTS public."User";
+-- DROP TABLE IF EXISTS public."user";
 
-CREATE TABLE IF NOT EXISTS public."User"
+CREATE TABLE IF NOT EXISTS public."user"
 (
     user_id bigint NOT NULL,
     user_name character varying(16) COLLATE pg_catalog."default" NOT NULL,
@@ -15,5 +15,5 @@ CREATE TABLE IF NOT EXISTS public."User"
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public."User"
+ALTER TABLE IF EXISTS public."user"
     OWNER to postgres;
